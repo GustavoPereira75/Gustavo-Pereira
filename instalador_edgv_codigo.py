5e4bc70d6aea8adb7f54e58eed9611afe7d74cb7
GustavoPereira75 <57115083+GustavoPereira75@users.noreply.github.com>
2023-03-28 20:30:26 -0300

    Add files via upload

---
+++ instalador_edgv_codigo.py
@@ -0,0 +1,13 @@
+import shutil
+import os
+
+# Caminho da pasta a ser copiada
+src_folder = './edgv_3_orto'
+
+# Caminho da pasta de destino
+dst_folder = os.path.join(os.getenv('APPDATA'), 'QGIS', 'QGIS3', 'profiles', 'default', 'processing' ,'models')
+
+# Copia a pasta
+shutil.copytree(src_folder, os.path.join(dst_folder, os.path.basename(src_folder)))
+
+print('Pasta copiada com sucesso!')
